05da789ba521e56ba94387d35bd8e71eb475ccae
Harsh Lakhani <harshlakhani@Harshs-MacBook-Pro.local>
2022-11-29 15:32:26 +0800

    add front-end

---
+++ Front-end/defi-react-api/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import './Navbar.css'
+import {
+    BrowserRouter,
+    Link
+  } from "react-router-dom";
+
+const Navbar = () => {
+const [click, setClick] = useState(false)
+const handleClick = () => setClick(!click)
+
+
+    return (
+        <div className='header'>
+            <div className='container'>
+                <Link to="/">
+                    <h1>Option<span className='primary'>DeFi</span></h1>
+                </Link>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li>
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/buyer">
+                            BEC
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/seller">
+                            Seller
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
